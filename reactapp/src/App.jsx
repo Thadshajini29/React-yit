c771eee4f11b32da5cef935fcdf68b8fc0284ad5
Thadshajini29 <sajiiselvi@gmail.com>
2025-12-16 11:29:12 +0530

    Initialize React app with Vite and migrate student components
    Set up a new React project using Vite, including configuration files, ESLint, and initial assets. Migrated student and subject components from styleapp to the new reactapp structure, renaming .js files to .jsx where appropriate.

---
+++ reactapp/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import Student from './components/student/Student'
+import Subject from './components/student/subject/Subject'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Student fname="Thadshajini" lname="Thadsha" grade="10B"><Subject /></Student>
+      <hr />
+      <Student fname="Kopitha" lname="Kopi" grade="11B"><Subject /></Student>
+      <hr />
+      <Student fname="Archchna" lname="Jena" grade="10A"><Subject /></Student>
+      <hr />
+      <Student fname="Sumistry" lname="Sumi" grade="11C"><Subject /></Student>
+      <hr />
+      <Student fname="Sankeetha" lname="Kavi" grade="10G"><Subject /></Student>
+      <hr />
+    </div>
+  )
+}
+
+export default App
