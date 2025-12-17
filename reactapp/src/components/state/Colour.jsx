654df13a84a1d6043c9a46a0dbc4a4cb1abefc98
Thadshajini29 <sajiiselvi@gmail.com>
2025-12-17 10:14:19 +0530

    Update Colour.jsx

--- reactapp/src/components/state/Colour.jsx
+++ reactapp/src/components/state/Colour.jsx
@@ -1,69 +1,69 @@
 import React, { useState } from 'react'
 
 export default function Colour() {
-    const [rcount, setrCount] = useState(255);
-    const [gcount, setgCount] = useState(0);
-    const [bcount, setbCount] = useState(255);
-    const colourcode = `rgb(${rcount},${gcount},${bcount})`;
+    const [redcount, setredcount] = useState(255);
+    const [greencount, setgreencount] = useState(0);
+    const [bluecount, setbluecount] = useState(0);
+    const colourcode = `rgb(${redcount},${greencount},${bluecount})`;
 
-    const MIN_VALUE = 0;
-    const MAX_VALUE = 255;
-    const STEP = 10;
+    const minimum_value = 0;
+    const maximum_value = 256;
+    const steps = 10;
 
     return (
         <>
             <div style={{
-                width: '100px',
+                width: '200px',
                 height: '100px',
                 backgroundColor: colourcode,
                 border: '1px solid black'
             }}>
             </div>
 
-            <h1>{rcount}</h1>
+            <h3>Red:{redcount}</h3>
             <button
                 onClick={() => {
-                    if (rcount + STEP <= MAX_VALUE) {
-                        setrCount((prevState) => prevState + STEP);
+                    if (redcount + steps < maximum_value) {
+                        setredcount((prevState) => prevState + steps);
                     }
                 }}>Increment
             </button>
             <button
                 onClick={() => {
-                    if (rcount - STEP >= MIN_VALUE) {
-                        setrCount((prevState) => prevState - STEP);
+                    if (redcount - steps > minimum_value) {
+                        setredcount((prevState) => prevState - steps);
                     }
                 }}>Decrement
             </button>
 
-            <h1>{gcount}</h1>
+            <h3>Green:{greencount}</h3>
             <button
                 onClick={() => {
-                    if (gcount + STEP <= MAX_VALUE) {
-                        setgCount((prevState) => prevState + STEP);
+                    if (greencount + steps < maximum_value) {
+                        setgreencount((prevState) => prevState + steps);
                     }
                 }}>Increment
             </button>
             <button
                 onClick={() => {
-                    if (gcount - STEP >= MIN_VALUE) {
-                        setgCount((prevState) => prevState - STEP);
+                    if (greencount - steps > minimum_value) {
+                        setgreencount((prevState) => prevState - steps);
                     }
                 }}>Decrement
             </button>
 
-            <h1>{bcount}</h1>
+            <h3>Blue:{bluecount}</h3>
             <button
                 onClick={() => {
-                    if (bcount + STEP <= MAX_VALUE) {
-                        setbCount((prevState) => prevState + STEP);
+                    if (bluecount + steps < maximum_value) {
+                        setbluecount((prevState) => prevState + steps);
                     }
                 }}>Increment
             </button>
             <button
                 onClick={() => {
-                    if (bcount - STEP >= MIN_VALUE) {
-                        setbCount((prevState) => prevState - STEP);
+                    if (bluecount - steps > minimum_value) {
+                        setbluecount((prevState) => prevState - steps);
                     }
                 }}>Decrement
             </button>
